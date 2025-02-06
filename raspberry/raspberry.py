ffd4e463bdcb85beafa044a3331c96ac2123b570
daniel <danikaszman@gmail.com>
2025-02-06 09:59:11 +0100

    Pi camera + tesseract hozzaadasa

--- raspberry/raspberry.py
+++ raspberry/raspberry.py
@@ -3,8 +3,16 @@ import os
 import cv2
 import re
 import pymongo
+import pytesseract
+from PIL import Image
+from picamera import PiCamera
+from time import sleep
 from google.cloud import vision
 from datetime import datetime
+
+#Pi camera elindítása (lehet hogy a mainbe kell tenni)
+camera = PiCamera()
+camera.start_preview()
 
 # Google hitelesítési kulcs (JSON fájl)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "raspberry/fridge-key.json"
@@ -16,14 +24,14 @@ db = client["product_database"]
 collection = db["products"]
 """
 
-def capture_image():
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    image_path = "product.jpg"
-    cv2.imwrite(image_path, frame)
-    cap.release()
-    return image_path
+def kep_keszitese():
+    image_path = '/home/pi/Desktop/image.jpg'
+    camera.capture(image_path)
+    sleep(1)
+    print(pytesseract.image_to_string(Image.open(image_path)))
+    return 
 
+"""
 def detect_text(image_path):
     client = vision.ImageAnnotatorClient()
     with io.open(image_path, 'rb') as image_file:
@@ -36,13 +44,16 @@ def detect_text(image_path):
     if texts:
         return texts[0].description
     return ""
+"""
 
+"""
 def extract_expiration_date(text):
     pattern = r"\b(\d{2}/\d{2}/\d{4}|\d{2}-\d{2}-\d{4}|\d{2}\.\d{2}\.\d{4})\b"
     match = re.search(pattern, text)
     return match.group(0) if match else "Nincs dátum"
+"""
 
-""""
+"""
 def save_to_mongodb(product_name, expiration_date):
     data = {
         "product_name": product_name,
@@ -54,9 +65,9 @@ def save_to_mongodb(product_name, expiration_date):
 """
     
 def main():
-    image_path = capture_image()
-    text = detect_text(image_path)
-    expiration_date = extract_expiration_date(text)
+    kep_keszitese()
+    #text = detect_text(image_path)
+    #expiration_date = extract_expiration_date(text)
     #save_to_mongodb(text, expiration_date)
     print(f"Felismert termék: {text}")
     print(f"Lejárati dátum: {expiration_date}")
